b8070d6c55a4221a4f668e93df7a23295010d375
Saplitsyn Denis <packman17@mail.ru>
2024-02-14 15:53:54 +0300

    add response with calling name

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -7,8 +7,22 @@ public class HelloWorld {
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
+
+        // запрос http://localhost:7070/users
         app.get("/users", ctx -> ctx.result("GET /users"));
         app.post("/users", ctx -> ctx.result("POST /users"));
+
+        // запрос http://localhost:7070/hello?name=jon
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParam("name");
+            if (name != null) {
+                ctx.result("Hello" + name + "!");
+            } else {
+                ctx.result("Hello, World!");
+            }
+        });
+
         app.start(7070);
+        //завершить запрос ctrl + c
     }
 }
