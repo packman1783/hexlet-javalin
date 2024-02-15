881fb2a3a2e5a5abc128ae0968a35096f3c01bd5
Saplitsyn Denis <packman17@mail.ru>
2024-02-15 23:11:21 +0300

    change for checkstyle

--- src/main/java/org/example/hexlet/CourseList.java
+++ src/main/java/org/example/hexlet/CourseList.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CourseList {
-    public static List<Course> createSampleCourseList() {
+    public static List<Course> CreatedList() {
         List<Course> courses = new ArrayList<>();
 
-        courses.add(new Course(1, "Introduction to", "Learn the basics of Java programming language"));
-        courses.add(new Course(2, "Web Development Fundamentals", "Explore the essentials of web development"));
-        courses.add(new Course(3, "Data Structures and Algorithms", "Master algorithms and data structures"));
+        courses.add(new Course(1, "Introduction", "Learn Java language"));
+        courses.add(new Course(2, "Web Development", "Explore web development"));
+        courses.add(new Course(3, "Data Algorithms", "Algorithms and structures"));
 
         return courses;
     }
